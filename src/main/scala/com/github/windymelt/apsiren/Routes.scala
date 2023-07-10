e6bb6390d3971b4aab12a7a850f43ca29ed0c028
windymelt <windymelt@3qe.us>
2023-07-11 00:04:43 +0900

    fixname

--- src/main/scala/com/github/windymelt/apsiren/Routes.scala
+++ src/main/scala/com/github/windymelt/apsiren/Routes.scala
@@ -41,7 +41,7 @@ class Routes(
 
   // If ask takes more time than this to complete the request is failed
   private implicit val timeout: Timeout = Timeout.create(
-    system.settings.config.getDuration("my-app.routes.ask-timeout")
+    system.settings.config.getDuration("sierrapub.routes.ask-timeout")
   )
 
   def follow(url: String): Future[Ok.type] =
