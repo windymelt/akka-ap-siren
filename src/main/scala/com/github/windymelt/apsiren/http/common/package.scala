f9bdd40071668475f32cfa71dfa3e6c6b37ae4aa
windymelt <windymelt@3qe.us>
2023-07-11 00:02:28 +0900

    refactor

--- src/main/scala/com/github/windymelt/apsiren/http/common/package.scala
+++ src/main/scala/com/github/windymelt/apsiren/http/common/package.scala
@@ -1,11 +1,11 @@
 package com.github.windymelt.apsiren.http
 
 import akka.http.scaladsl.model.ContentType
+import akka.http.scaladsl.model.ContentTypes
 import akka.http.scaladsl.model.HttpCharsets
-import akka.http.scaladsl.model.MediaType
 import akka.http.scaladsl.model.HttpEntity
 import akka.http.scaladsl.model.HttpRequest
-import akka.http.scaladsl.model.ContentTypes
+import akka.http.scaladsl.model.MediaType
 
 package object common {
   val activityCT = ContentType.WithFixedCharset(
@@ -21,23 +21,16 @@ package object common {
 
     val bytes = j.asJson.noSpaces.getBytes()
 
-    HttpEntity(
-      activityCT,
-      bytes
-    )
+    HttpEntity(activityCT, bytes)
   }
   def activityRequest[A: io.circe.Encoder](
       j: A
   ): akka.http.scaladsl.model.RequestEntity = {
     import io.circe.syntax._ // for asJson
 
-    println(j.asJson.noSpaces)
     val bytes = j.asJson.noSpaces.getBytes()
 
-    HttpEntity(
-      activityCT,
-      bytes
-    )
+    HttpEntity(activityCT, bytes)
   }
   def activityAsJson(req: HttpRequest): HttpRequest = {
     req.withEntity(req.entity.withContentType(ContentTypes.`application/json`))
