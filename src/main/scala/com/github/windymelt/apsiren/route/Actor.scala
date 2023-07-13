454dc906b392a2efd8f11c9eb82cbf7ee970b198
windymelt <windymelt@3qe.us>
2023-07-14 01:03:41 +0900

    move to config

--- src/main/scala/com/github/windymelt/apsiren/route/Actor.scala
+++ src/main/scala/com/github/windymelt/apsiren/route/Actor.scala
@@ -1,6 +1,7 @@
 package com.github.windymelt.apsiren
 package route
 
+import akka.actor.typed.ActorSystem
 import akka.event.Logging
 import akka.http.scaladsl.model.HttpResponse
 import akka.http.scaladsl.server.Directives._
@@ -9,11 +10,13 @@ import akka.http.scaladsl.server.Route
 import http.common._
 
 object Actor {
-  val route: Route = path("actor") {
+  def route(implicit system: ActorSystem[Nothing]): Route = path("actor") {
     logRequestResult(("actor", Logging.InfoLevel)) {
       get {
         complete {
           import io.circe.syntax._
+          val pubkey =
+            system.settings.config.getString("sierrapub.server.publicKey")
 
           val actor = model.Actor(
             id = "https://siren.capslock.dev/actor",
@@ -34,24 +37,11 @@ object Actor {
               publicKeyPem = pubkey.replaceAll(lf, "\\\n")
             )
           )
-
           HttpResponse(entity = activity(actor))
         }
       }
     }
   }
-// openssl genrsa -out server.key 2048
-// openssl rsa -in server.key -pubout
-  val pubkey = """-----BEGIN PUBLIC KEY-----
-MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAryP6P3P4X1qNJAIyg9Q4
-eAJdzdMBD7oFPqqsyurnWskvCMeljM6sxoohnrVEjD10NZirnyt7X/cpYSc5BMGk
-wIfTWyhTMYbNTXlrV0yFrsBtv39tG5TcEWdX1+NvMn68MsCkLv7h/qsz4rBVxmmf
-c0lpz9KCqv1AI3mSuJYVNEXP59QuoP0jqtxE2e4Man4hp/BU26XBJJ8i/ZshrXtb
-3/3A7K60cYjCboTDwCzD4TYuxgwx0Jgk28zlTYM1NuQNYehpgd5mviUXdFdWatuP
-WSuAjGu0T6RNMWTcUh0cV39+wr1fKtZ9rHPubxXz7eikOGvjbB8UIDjXG5Kh7Xv3
-WwIDAQAB
------END PUBLIC KEY-----
-"""
   val lf = """
 """
 }
