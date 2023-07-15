3c5c4740b7e5325341e1eadac19cbf8f9931ea30
windymelt <windymelt@3qe.us>
2023-07-16 02:41:13 +0900

    Allow bulk-insert followers

--- src/main/scala/com/github/windymelt/apsiren/route/InternalComponent.scala
+++ src/main/scala/com/github/windymelt/apsiren/route/InternalComponent.scala
@@ -1,34 +1,57 @@
 package com.github.windymelt.apsiren
 package route
 
+import akka.actor.typed.scaladsl.AskPattern._
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.Route
-import http.common.checkBearerToken
-import akka.actor.typed.scaladsl.AskPattern._
 import akka.util.Timeout
-import scala.concurrent.duration.FiniteDuration
 import de.heikoseeberger.akkahttpcirce.FailFastCirceSupport._
+
+import scala.concurrent.ExecutionContext
+import scala.concurrent.Future
+import scala.concurrent.duration.FiniteDuration
+import scala.util.Failure
+import scala.util.Success
+
+import http.common.checkBearerToken
 
 trait InternalComponent {
   self: ActorSystemComponent with repo.FollowersComponent =>
 
   implicit private val timeout: Timeout =
-    Timeout.durationToTimeout(FiniteDuration(1, "second"))
+    Timeout.durationToTimeout(FiniteDuration(30, "second"))
 
   val internalRoute: Route =
     pathPrefix("internal") {
       path("followers") {
-        get {
-          authenticateOAuth2(
-            "internal",
-            checkBearerToken(
-              system.settings.config.getString("sierrapub.post.bearer")
-            )
-          ) { _ =>
+        authenticateOAuth2(
+          "internal",
+          checkBearerToken(
+            system.settings.config.getString("sierrapub.post.bearer")
+          )
+        ) { _ =>
+          get {
             onSuccess(followersRepository.ask(protocol.Followers.GetAll(_))) {
               followers =>
                 import io.circe.generic.auto._
                 complete(followers)
+            }
+          } ~ post {
+            import io.circe.generic.auto._
+            entity(as[protocol.Followers.Followers]) { followers =>
+              implicit val ec: ExecutionContext = system.executionContext
+              val adds = Future.sequence(
+                followers.followers.toSeq
+                  .map(f =>
+                    followersRepository.ask(protocol.Followers.Add(f, _))
+                  )
+              )
+              onComplete(adds) {
+                case Failure(exception) =>
+                  complete(500 -> exception.getMessage())
+                case Success(value) =>
+                  complete("ok")
+              }
             }
           }
         }
